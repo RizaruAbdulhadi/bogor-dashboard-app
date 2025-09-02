97bf009392696db27dec2063156486753f3238d4
RizaruAbdulhadi <cerberus16121992@gmail.com>
2025-09-02 12:45:21 +0700

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'http://192.168.1.101:5000/api',
 });
 
 // Interceptor untuk menambahkan token
