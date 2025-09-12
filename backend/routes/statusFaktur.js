71acdea5184ed3cbc41e7696c6749c863a0a4814
RizaruAbdulhadi <cerberus16121992@gmail.com>
2025-09-12 10:08:57 +0700

    Update statusFaktur.js

--- backend/routes/statusFaktur.js
+++ backend/routes/statusFaktur.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const multer = require('multer');
 const { uploadExcel, getAllFaktur, getAgingHD } = require('../controllers/statusFakturController');
 
-const upload = multer({ dest: 'upload/' });
+const upload = multer({ dest: 'uploads/' });
 
-router.post('/upload', upload.single('file'), uploadExcel);
+router.post('/uploads', upload.single('file'), uploadExcel);
 router.get('/', getAllFaktur);
 router.get('/aging-hd', getAgingHD);
 
